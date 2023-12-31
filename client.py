ab0b123a08cf927a7cf4deacc7d1ccaf3ed021a4
aeaaym <153832180+aeaaym@users.noreply.github.com>
2023-12-31 22:11:21 +0300

    We added a working GUI and fixed a few errors

--- client.py
+++ client.py
@@ -1,54 +1,132 @@
 import socket
+import customtkinter
 
-
-#To make sure all the data is received
-def receive_large_data(sock):
-    BUFF_SIZE = 4096 # 4 KiB
-    data = b''
+def receive_large_responses(socket):
+    response = b''
     while True:
-        part = sock.recv(BUFF_SIZE)
-        data += part
-        if len(part) < BUFF_SIZE:
-            # either 0 or end of data
+        part = socket.recv(4096)
+        response += part
+        if len(part) < 4096:
             break
-    return data
+    return response
 
+def name_enter_button():
+    try:
+        #gets the client name from the Entry box "name".
+        client_name=name.get()
+        if client_name:
+            # connect to the server
+            client.connect(("127.0.0.1", 9999)) # connect to the server
+            
+            client.send(client_name.encode('ascii'))
+            #remove the frame for the name entry and show the frame for the rest of the options.
+            frame1.pack_forget()
+            frame.pack(expand="true")
+        else:
+            #displays the text in red if the user has clicked the button without entering a name.
+            name_error =customtkinter.CTkLabel(frame1, text="Please Enter Your Name To Continue", font=("", 20), text_color="red")
+            name_error.grid(column=1, row=2)
+    except:
+        print("Error can't communicate with the server")
+        app.destroy()
+
+def send_option_to_server(choice):
+    try:
+        #sends the choices and the input if applicable to the server.
+        if choice == 'c':
+            input = opt_C_IATA.get()
+            if not input:
+                opt_C_IATA.configure(placeholder_text_color="red", font=('',17))
+            else:
+                client.send(choice.encode('ascii'))
+                client.send(input.encode('ascii'))
+                opt_C_IATA.configure(placeholder_text_color="gray", font=('',14))
+                opt_C_IATA.delete("0","end")
+                receive_and_print_data(choice)
+        elif choice == 'd':
+            input = opt_D_IATA.get()
+            if not input:
+                opt_D_IATA.configure(placeholder_text_color="red", font=('',17))
+            else:
+                client.send(choice.encode('ascii'))
+                client.send(input.encode('ascii'))
+                opt_D_IATA.configure(placeholder_text_color="gray", font=('',14))
+                opt_D_IATA.delete("0","end")
+                receive_and_print_data(choice)
+        else:
+            client.send(choice.encode('ascii'))
+            receive_and_print_data(choice)
+    except:
+        print("Error can't communicate with the server")
+        app.destroy()
+
+def receive_and_print_data(choice):
+    #calls receive_large_responses function then printing the response in the textbox.
+    received_data = receive_large_responses(client)
+    received_data=received_data.decode('ascii')
+    
+    textbox.configure(state="normal")
+    textbox.delete('1.0', 'end')
+    textbox.insert("end", f"Server's response for option {choice.upper()}: \n")
+    textbox.insert("end", received_data)
+    textbox.configure(state="disabled")
+
+def quit_command():
+    #closes the connection and gui if user presses quit button.
+    client.close()
+    app.destroy()
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+
+######################################################################################################################################
+customtkinter.set_appearance_mode("light")
+customtkinter.set_default_color_theme("blue")
+
+app = customtkinter.CTk()
+app.geometry("720x480")
+app.title("ITNE352")
+
+#Frames for the username window and options window
+frame = customtkinter.CTkFrame(app, fg_color="transparent")
+frame1 = customtkinter.CTkFrame(app, fg_color="transparent")
+
+quit_button = customtkinter.CTkButton(app, text= "quit", command=quit_command, width = 60, height = 20)
+quit_button.pack(anchor = "nw")
+
+#client's name entry box and enter button
+name = customtkinter.CTkEntry(frame1, placeholder_text="Enter your name",font=("",18), width=250, height=60,)
+name.grid(column=1, row=0, pady=10,)
+button=customtkinter.CTkButton(frame1, text="Enter", font=("",18), width=250, height=60, command=name_enter_button)
+button.grid(column=1, pady=10, row=1)
+frame1.pack(expand="true")
+
+
+#options buttons and input boxes
+button1=customtkinter.CTkButton(frame, font=("",14), text="A. All Arrived Flights",width=230, height=40, command=lambda: send_option_to_server('a'))
+button1.grid(column=0, pady=10, row=2, padx=30 )
+
+button2=customtkinter.CTkButton(frame, font=("",14), text="B. All Delayed Flights", width=230, height=40, command=lambda: send_option_to_server('b'))
+button2.grid(column=0, pady=10, row=4, padx=30)
+
+opt_C_IATA = customtkinter.CTkEntry(frame, font=("",14), placeholder_text="Enter The Airport's IATA Code", width=230, height=40)
+opt_C_IATA.grid(column=2,row=3, padx=30)
+
+button3=customtkinter.CTkButton(frame, font=("",14), text="C. All Flights From a Specific Airport", width=230, height=40, command=lambda: send_option_to_server('c'))
+button3.grid(column=2, row=2, padx=30)
+
+opt_D_IATA = customtkinter.CTkEntry(frame, font=("",14), placeholder_text="Enter The Flight's IATA Code", width=230, height=40)
+opt_D_IATA.grid(column=2,  row=5, padx=30)
+
+button4=customtkinter.CTkButton(frame, font=("",14), text="D. Details of a Particular Flight", width=230, height=40, command=lambda: send_option_to_server('d'))
+button4.grid(column=2, row=4, padx=30)
+
+#response box 
+textbox= customtkinter.CTkTextbox(frame, width=720)
+textbox.configure(state="disabled")
+textbox.grid(column=0, columnspan=3, row=6, pady = 30)
+
 try:
-    client.connect(("127.0.0.1", 9999)) # connect to the server
+    app.mainloop()
+
 except:
-    print("cant find server !") # if cant be found then the client will shutdown
-    exit(0)
-
-
-
-name = input("Enter your name: ") # gets the client name from user 
-client.send(name.encode('ascii')) # send name to the server after encoding it
-
-
-print(f"------------Welcome {name}-------------")
-while True:
-    recv_option = client.recv(2085).decode()
-
-    print(recv_option)
-    choice=input("please Enter your choice: ")
-    client.send(choice.encode('ascii'))
-    
-    if choice.lower() == 'c':
-        choice_c=input("ENTER city code: ")
-        client.send(choice_c.encode('ascii'))
-
-    elif choice.lower()=='d':
-        choice_d=input("Enter Details of a particular flight: ")
-        client.send(choice_d.encode('ascii'))
-    
-    # quit option
-    if choice.lower() == 'e':
-        break
-    
-    #print the received data
-    received_data = receive_large_data(client)
-    received_data=received_data.decode('ascii')
-    print(received_data,'\n')
-client.close()
+    print("Error client terminated")
--- server.py
+++ server.py
@@ -4,16 +4,16 @@ import requests
 import json
 
 
-def get_data(client_socket, recv_option):
-
-    #open the json file to extract data
+def send_option(client_socket, recv_option, name):
+    
     with open("GA8.json", 'r') as file:
         data = json.load(file)
         flights = data.get('data', [])
 
     if recv_option.lower() == 'a':
-        # Extract the required information for arrived flights
-        arrived_flights = []
+        print(f'client: {name} chose option: A. All Arrived Flights ')
+        # Extract the required information for all arrived flights
+        data = []
         for flight in flights:
             if flight['flight_status'] == 'landed':
                 flight_info = {
@@ -23,12 +23,16 @@ def get_data(client_socket, recv_option):
                     'Arrival Terminal Number': flight['arrival']['terminal'],
                     'Arrival Gate': flight['arrival']['gate']
                 }
-                arrived_flights.append(flight_info)
-        return json.dumps(arrived_flights, indent=2)
-
+                data.append(flight_info)
+        if data:
+            return json.dumps(data, indent=2)
+        else:
+            return "No Arrived Flights found."
+       
     elif recv_option.lower() == 'b':
-        # Extract the required information for delayed flights
-        delayed_flights = []
+        print(f'client: {name} chose option: B. All Delayed Flights ')
+        # Extract the required information for all arrived flights
+        data = []
         for flight in flights:
             if flight['arrival']['delay'] != None:
                 flight_info = {
@@ -40,12 +44,16 @@ def get_data(client_socket, recv_option):
                     'Delay': flight['arrival']['delay'],
                     'Arrival Gate': flight['arrival']['gate']
                 }
-                delayed_flights.append(flight_info)
-        return json.dumps(delayed_flights, indent=2)
+                data.append(flight_info)
+        if data:
+            return json.dumps(data, indent=2)
+        else:
+            return "No Delayed Flights found."
 
     elif recv_option.lower() == 'c':
         # Extract the required information for all flights from a specific airport
         airport_code = client_socket.recv(1024).decode("ascii")
+        print(f'client: {name} chose option: C. All Flights From Airport {airport_code}')
         data = []
         for flight in flights:
             if flight['departure']['iata'] == airport_code:
@@ -62,9 +70,12 @@ def get_data(client_socket, recv_option):
         if data:
             return json.dumps(data, indent=2)    
         else:
-            return "Airport not found."
+            return f"Airport {airport_code} not found."
+        
+
     elif recv_option.lower() == 'd':
         flight_iata_code = client_socket.recv(1024).decode().strip()
+        print(f'client: {name} chose option: D. Details of Flight Number {flight_iata_code}')
         data = []
         for flight in flights:
             if flight['flight']['iata'] == flight_iata_code:
@@ -84,43 +95,25 @@ def get_data(client_socket, recv_option):
         if data:
             return json.dumps(data, indent=2)
         else:
-            return "Flight not found."
-
-    else:
-        client_socket.send(b"Invalid option. Please choose again.")
+            return f"Flight {flight_iata_code} not found."
 
 
-def clients_handler(client_socket, name):
-    while True:
-        #send the options to the client
-        client_socket.send(b"Choose on of the following Options: \n"
-                           b"a. ALL Arrived Flights \n"
-                           b"b. ALL Delayed Flights \n"
-                           b"c. ALL Flight From a Specific Airport \n"
-                           b"d. Details Of a Particular Flight \n"
-                           b"e. to terminate the connection")
-
-        # receive the client's choice 
-        recv_option = client_socket.recv(1024).decode('ascii')
-
-        #option 'e' to close the connection and print that the client has disconnected
-        if recv_option.lower() == 'e':
-            client_socket.close()
-            print(f"client: {name} has disconnected")
-            return
-        
-        print(f'client: {name} chose option: {recv_option}')
-        response = get_data(client_socket, recv_option)#get the desired data using the option from the client
-        client_socket.send(response.encode('ascii'))#send the data to the client
-        print(65*'-')
-        print(response)#for testing and development
-        print(65*'-')
-
+def clients_handler(client_socket, addr):
+    try:
+        #Receives the client name.
+        name = client_socket.recv(1024).decode('ascii')
+        print(f"accepted connection with {addr} client's name is: {name}")
+        while True:
+            # to send the option
+            recv_option = client_socket.recv(1024).decode('ascii')
+            response = send_option(client_socket, recv_option, name)
+            client_socket.sendall((response).encode('ascii'))   
+    except :
+        print(f"connection with {name} at address {addr} has been disconnected")
 
 def main():
-    #the code to retrieve the data from the api commented for development and testing 
     
-    airport_code = input("Enter airport ICAO code: ")
+    """airport_code = input("Enter the airport's ICAO code: ")
 
     api_key = '97cb3a487d121f559d1dea7e9565fe33'
     url = f'http://api.aviationstack.com/v1/flights?access_key={api_key}&arr_icao={airport_code}&limit=100'
@@ -129,9 +122,8 @@ def main():
     data = response.json()
 
     with open("GA8.json", 'w') as file:
-        json.dump(data, file, indent=4)
-
-    #create the socket and start listening 
+        json.dump(data, file, indent=4)"""
+        
     server4 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server4.bind(('127.0.0.1', 9999))
     server4.listen()
@@ -139,14 +131,10 @@ def main():
 
     while True:
         client_socket, addr = server4.accept()
-        print("Client connected:", addr)
-        # Receive the name from the client
-        name = client_socket.recv(1024).decode('ascii')
-        print(f"Client: {name} successfully connected")
-        #thread to be able to handle multiple clients
-        clients_thread = threading.Thread(target=clients_handler, args=(client_socket, name))
+
+        #thread to handle multiple clients.
+        clients_thread = threading.Thread(target=clients_handler, args=(client_socket, addr))
         clients_thread.start()
-
-
+        
 if __name__ == "__main__":
     main()
